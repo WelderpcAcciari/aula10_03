eb5686b6a2f7b7b01df73ecac3dc2ee954362812
0220482213012 <0220482213012@LAB4MAQ13>
2023-03-10 19:52:32 -0300

    Add project files.

---
+++ aula10_03/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula10_03
+{
+    public class Produto
+    {
+
+        private int Id;
+        private decimal Valor;
+        private string Descricao;
+
+        public Produto(int id, decimal valor, string descricao)
+        {
+            Id = id;
+            Valor = valor;
+            Descricao = descricao;
+        }
+    }
+}
---
+++ aula10_03/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Produto p1 = new Produto(1, 10.90m, "cervejinha");
+
+Console.WriteLine("");
